3f7b589ab67736643fcc111afa4c444277f4b75f
Ihar Skavysh <igogoblin@gmail.com>
2024-09-26 22:32:19 +0300

    feat: add holiday section in main page

--- src/app/components/MainPage/HolidaysItem.tsx
+++ src/app/components/MainPage/HolidaysItem.tsx
@@ -1,15 +1,18 @@
 import styles from './holidays.module.css';
 import Image, { StaticImageData } from 'next/image';
-
+import imageCap from '../../../../public/mainHolidaysCap.png';
 type HolidaysItemProps = {
-  id?: number;
+  id: number;
   text: string;
   image: StaticImageData;
 };
 
-function HolidaysItem({ text, image }: HolidaysItemProps) {
+function HolidaysItem({ id, text, image }: HolidaysItemProps) {
   return (
     <div className={styles.holidays_item}>
+      {id === 0 && (
+        <Image src={imageCap} alt={'cap'} width={60} height={45} className={styles.cap} />
+      )}
       <Image src={image} alt={'doughnut'} width={60} height={45} />
       <span>{text}</span>
     </div>
