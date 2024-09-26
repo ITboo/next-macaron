009cae7a4d7e0101de70dd36cc68bfdf5fbca1d3
Infared! <ekkirkitadze@gmail.com>
2024-09-26 21:30:04 +0300

    feat: create markup and styles

--- src/app/pages/not-found/page.tsx
+++ src/app/pages/not-found/page.tsx
@@ -1,13 +1,47 @@
-//import styles from './page.module.css';
+import styles from './page.module.css';
+import Image from 'next/image';
+// const CookieImg = () => {
+//   return (
+//       <Image
+//           src="/cookie.png" 
+//           alt="Sad cookie"
+//           width={321} 
+//           height={321} 
+//       />
+//   );
+// };
+
 
 import Container from '@/app/components/Container';
 
 const NotFound = () => {
   return (
     <section>
-      <Container></Container>
+      <div className={styles.wrapper_not_found}>
+      <Container>
+          <p className={styles.text_not_found}>Извините, страница не найдена</p>
+          <div className={styles.wrapper_error}>
+          <div className={styles.error}>
+          
+
+          <div className={styles.images_cookies}>
+            {/* <CookieImg /> */}
+            <Image src='/cookie.png' alt='sad cookie' width={321} height={321}/>
+          {/* <Image 
+          src="/cookie.png" 
+          alt={'Sad cookie'} 
+          width={321} 
+          height={321}/> */}
+          </div>
+          </div>
+          <button className={styles.back_button}>Назад</button>
+        </div>
+      </Container>
+      </div>
     </section>
   );
 };
 
 export default NotFound;
+
+
