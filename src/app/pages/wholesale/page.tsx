937805928a466c1ef7e8f0362cbf3b9e0d5268a7
koonukaame <marialezhebokova@yandex.ru>
2024-09-27 18:10:05 +0300

    feat: add wholesale-ffer markup and styles

--- src/app/pages/wholesale/page.tsx
+++ src/app/pages/wholesale/page.tsx
@@ -4,7 +4,7 @@ import Container from '@/app/components/Container';
 
 const Wholesale = () => {
   return (
-    <section>
+    <section className={styles.wholesale}>
       <Container>
         <div className={styles.breadcrumbs}>
           <span>Главная</span>
@@ -30,6 +30,43 @@ const Wholesale = () => {
                 <li>продукцию для дегустации или снижение цены для проведения промо-акций;</li>
                 <li>гибкие условия сотрудничества и поставок.</li>
               </ul>
+            </div>
+          </div>
+        </div>
+        <div className={styles['wholesale-offer']}>
+          <h3 className={styles['minor-heading']}>Что мы можем вам предложить:</h3>
+          <div className={styles['wholesale-offer__options']}>
+            <div className={styles['wholesale-offer__option']}>
+              <div className={[styles['wholesale-offer__option-background'], styles.gifts].join(' ')}></div>
+              <span>Корпоративные<br></br>подарки</span>
+            </div>
+            <div className={styles['wholesale-offer__option']}>
+              <div className={[styles['wholesale-offer__option-background'], styles.eshops].join(' ')}></div>
+              <span>Сотрудничество<br></br>с интернет магазинами</span>
+            </div>
+            <div className={styles['wholesale-offer__option']}>
+              <div className={[styles['wholesale-offer__option-background'], styles.cafes].join(' ')}></div>
+              <span>Сотрудничество<br></br>с кафе и ресторанами</span>
+            </div>
+            <div className={styles['wholesale-offer__option']}>
+              <div className={[styles['wholesale-offer__option-background'], styles.coffeeshops].join(' ')}></div>
+              <span>Сотрудничество<br></br>с кофейнями</span>
+            </div>
+            <div className={styles['wholesale-offer__option']}>
+              <div className={[styles['wholesale-offer__option-background'], styles.hotels].join(' ')}></div>
+              <span>Сотрудничество с отелями</span>
+            </div>
+            <div className={styles['wholesale-offer__option']}>
+              <div className={[styles['wholesale-offer__option-background'], styles.retail].join(' ')}></div>
+              <span>Для Retail</span>
+            </div>
+            <div className={styles['wholesale-offer__option']}>
+              <div className={[styles['wholesale-offer__option-background'], styles.candybars].join(' ')}></div>
+              <span>Кенди бары для мероприятий</span>
+            </div>
+            <div className={styles['wholesale-offer__option']}>
+              <div className={[styles['wholesale-offer__option-background'], styles.otherspheres].join(' ')}></div>
+              <span>И для других сфер бизнеса</span>
             </div>
           </div>
         </div>
