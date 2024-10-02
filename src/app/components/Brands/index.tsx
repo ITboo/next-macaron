a9425980e6a4cfe0c6c7686d185555d2dd7452a0
koonukaame <marialezhebokova@yandex.ru>
2024-10-02 13:43:35 +0300

    feat: create brands component

---
+++ src/app/components/Brands/index.tsx
@@ -0,0 +1,47 @@
+import styles from './brands.module.css';
+
+const brands = [
+  {
+    className: 'metro',
+    id: 0
+  },
+  {
+    className: 'zenit',
+    id: 1
+  },
+  {
+    className: 'mvideo',
+    id: 2
+  },
+  {
+    className: 'gazprom',
+    id: 3
+  },
+  {
+    className: 'zarina',
+    id: 4
+  }
+];
+
+
+const Brands = () => {
+  return (
+    <>
+      <div className={styles['wholesale-review__brands']}>
+        {brands.map((brand) => (
+          <div
+            key={brand.id}
+            className={[styles.brand, styles[brand.className]].join(' ')}
+          ></div>
+        ))}
+      </div>
+      <div className={styles['pagination']}>
+        <div className={[styles['pagination__element'], styles['_current']].join(' ')}></div>
+        <div className={styles['pagination__element']}></div>
+        <div className={styles['pagination__element']}></div>
+      </div>
+    </>
+  )
+}
+
+export default Brands;
