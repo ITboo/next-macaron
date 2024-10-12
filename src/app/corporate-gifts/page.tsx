80a88590d93221ac8bb81a646803da008eb12737
koonukaame <marialezhebokova@yandex.ru>
2024-10-12 22:59:21 +0300

    feat: create gifts-variant markup and styles

--- src/app/corporate-gifts/page.tsx
+++ src/app/corporate-gifts/page.tsx
@@ -4,6 +4,34 @@ import Container from '@/app/components/Container';
 import Breadcrumb from '../shared/ui/Breadcrumb';
 // import Link from 'next/link';
 
+const giftVariants = [
+  {
+    heading: '3 макарона с печатью',
+    description: 'В пластиковой упаковке самый бюджетный вариант',
+    price: 'от 150 руб/шт',
+    className: 'pic_threeMacaroons',
+    key: 0,
+  }, {
+    heading: '9 макарон с печатью',
+    description: 'С логотипом Вашей копании, пожеланиями и т.д.',
+    price: 'от 500 руб',
+    className: 'pic_nineMacaroons',
+    key: 1,
+  }, {
+    heading: 'Круглый набор, 40 шт.',
+    description: 'Для самых любимых и дорогих клиентов',
+    price: 'от 3600 руб',
+    className: 'pic_fortyMacaroons',
+    key: 2,
+  }, {
+    heading: 'Набор на 70 шт.',
+    description: 'Роскошный подарок для большой компании',
+    price: 'от 5600 руб',
+    className: 'pic_seventyMacaroons',
+    key: 3,
+  },
+];
+
 const CorporateGifts = () => {
   return (
     <section className={styles['corporate-gifts']}>
@@ -23,6 +51,26 @@ const CorporateGifts = () => {
             <button className={styles['corporate-gifts__btn']}>Скачать весь каталог подарков</button>
           </div>
         </div>
+      </Container>
+      <Container>
+        <div className={styles['gift-variants']}>
+          <h3 className={styles['minor-heading']}>Некоторые варианты подарков</h3>
+          <div className={styles['gifts-variants__container']}>
+            {giftVariants.map((variant) => (
+              <div className={styles['gift-variant']} key={variant.key}>
+            <div className={[styles['gift-variant__pic'], styles[variant.className]].join(' ')}></div>
+            <div className={styles['gift-variant__text-content']}>
+              <span className={styles['gift-variant__heading']}>{variant.heading}</span>
+            <span className={styles['gift-variant__description']}>{variant.description}</span>
+            <div className={styles.separator}></div>
+            <span className={styles['gift-variant__price']}>{variant.price}</span>
+            </div>
+          </div>
+          ))}
+        </div>
+        <button className={styles['gift-variant__btn']}>Получить КП</button>
+        </div>
+        
       </Container>
     </section>
   )
